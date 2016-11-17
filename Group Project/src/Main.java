f1c537080e78019eea7bdbc01ca537392e0c160d
CookiesIceCream <lf5v2.99@gmail.com>
2016-11-17 15:50:38 +0800

    Update Main.java

--- Group Project/src/Main.java
+++ Group Project/src/Main.java
@@ -2,98 +2,180 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 
 public class Main 
 {
 	public enum foodType
 	{
-		APPRETIZER, DESSERT, DRINKS, MAINDISH
+		APPETIZER, DESSERT, DRINKS, MAINDISH
 	}
-	
+
 	private static Map<String, String> userInput = new HashMap<String,String>();
-	private static ArrayList<ArrayList<Food>> menu = new ArrayList<ArrayList<Food>>();
-	private static ArrayList<Food> appretizerList = new ArrayList<Food>();
-	private static ArrayList<Food> mainDishList = new ArrayList<Food>();
-	private static ArrayList<Food> drinkList = new ArrayList<Food>();
-	private static ArrayList<Food> dessertList = new ArrayList<Food>();
-	
-	public static Map<String, String> getUserInput()
-	{
-		// Import user input
-		Scanner input = new Scanner(System.in);
-		boolean appretizer;
-		boolean mainDish;
-		boolean drink;
-		boolean dessert;
-		double budget;
-		
-		System.out.println("Would you like to order appretizer?");
-		appretizer = input.nextBoolean();
-		System.out.println("Would you like to order main dish?");
-		mainDish = input.nextBoolean();
-		System.out.println("Would you like to order drink?");
-		drink = input.nextBoolean();
-		System.out.println("Would you like to order dessert?");
-		dessert = input.nextBoolean();
-		System.out.println("Please enter your budget");
-		budget = input.nextDouble();
-		userInput.put("appretizer",String.valueOf(appretizer));
-		userInput.put("mainDish",String.valueOf(mainDish));
-		userInput.put("drink",String.valueOf(drink));
-		userInput.put("dessert",String.valueOf(dessert));
-		userInput.put("budget", String.valueOf(budget));
-	
-		return userInput;
-	}
-	
-	public static void importMenu() throws FileNotFoundException
-	{
-		Scanner inFile = new Scanner(new File("Food_Data.txt"));
-		while(inFile.hasNextLine())
-		{
-			String input = inFile.nextLine();
-			input.trim();
-			String[] inputArray = input.split(",");
-			if (inputArray[0].equals(foodType.APPRETIZER.toString()))
-			{
-				appretizerList.add(new Appretizer(inputArray[1],foodType.APPRETIZER,inputArray[2]));
-			}
-			else if (inputArray[0].equals(foodType.DESSERT.toString()))
-			{
-				dessertList.add(new Appretizer(inputArray[1],foodType.DESSERT,inputArray[2]));
-			}
-			else if (inputArray[0].equals(foodType.MAINDISH.toString()))
-			{
-				mainDishList.add(new Appretizer(inputArray[1],foodType.MAINDISH,inputArray[2]));
-			}
-			else if (inputArray[0].equals(foodType.DRINKS.toString()))
-			{
-				drinkList.add(new Appretizer(inputArray[1],foodType.DRINKS,inputArray[2]));
-			}
-		}
-		System.out.println("Import completed!!");
-	}
-	
+	private static ArrayList<ArrayList<? extends Food>> menu = new ArrayList<ArrayList<? extends Food>>();
+	private static ArrayList<? extends Food> appetizerList = new ArrayList<Appetizer>();
+	private static ArrayList<? extends Food> mainDishList = new ArrayList<MainDish>();
+	private static ArrayList<? extends Food> drinkList = new ArrayList<Drinks>();
+	private static ArrayList<? extends Food> dessertList = new ArrayList<Dessert>();
+	private static ArrayList<ArrayList<Food>> tmpFoodCombination = new ArrayList<ArrayList<Food>>();
+	private static ArrayList<Combination> tmpFoodCombinationList = new ArrayList<Combination>();
+	private static double tempCombinationPrice = 0;
 	public static void main(String arg[]) 
 	{
-		
+
 		try {
-			importMenu();
+
+			ImportMenu getMenu = new ImportMenu();
+			getMenu.importMenu();
+
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found...exiting program...");
 			System.exit(0);
 		}
-		getUserInput();
+
+		ImportUserInput getUserInput = new ImportUserInput();
+		userInput=getUserInput.importUserInput();
+
+		if(userInput.get("appretizer").equals("true"))
+			menu.add(appetizerList);
+		if(userInput.get("mainDish").equals("true"))
+			menu.add(mainDishList);
+		if(userInput.get("drink").equals("true"))
+			menu.add(drinkList);
+		if(userInput.get("dessert").equals("true"))
+			menu.add(dessertList);
+
+		calculatePrice(menu, userInput);
 		
-		menu.add(appretizerList);
-		menu.add(mainDishList);
-		menu.add(drinkList);
-		menu.add(dessertList);
-		
-		//calculatePrice(menu, budget);
+		for(Combination c :tmpFoodCombinationList)
+		{
+			c.getCombinationPrice();
+			System.out.println();
+		}
 	}
-	
+
+	public Map<String, String> getUserInput()
+	{
+		return userInput;
+	}
+
+	public ArrayList<ArrayList<? extends Food>> getMenu()
+	{
+		return menu;
+	}
+
+
+	private static void calculatePrice(ArrayList<ArrayList<? extends Food>> menu2, Map<String, String> userInput2) {
+		int currentMenuCounter=0;
+		double tempPrice = 0;
+		ArrayList<Food> foodStorageList = new ArrayList<Food>();
+
+		for (Food f: menu.get(0))
+		{
+			if (Double.parseDouble(f.getPrice()) > Double.parseDouble(userInput2.get("budget")))
+			{
+				continue;
+			}
+			else
+			{
+				tempCombinationPrice += Double.parseDouble(f.getPrice());
+			}
+			if (foodStorageList.isEmpty())
+			{
+				foodStorageList.add(f);
+			}
+			else
+			{
+				foodStorageList.clear();
+				foodStorageList.add(f);
+			}
+			
+			if(menu.size() == 0)
+			{
+				System.out.println("No combination found!!!");
+			}
+			if(menu.size() == 1)
+			{
+				if (tempCombinationPrice <= Double.parseDouble(userInput2.get("budget")) && tempPrice> 0)
+				{
+					tmpFoodCombinationList.add(new Combination(foodStorageList));
+					continue;
+				}
+			}
+			else
+			{
+				calculateCombination(currentMenuCounter, foodStorageList);
+				
+				if (tempCombinationPrice < 0)
+				{
+					System.out.println("Shit happens!!!");
+				}
+				foodStorageList = new ArrayList<Food>();
+				tempPrice = 0;
+			}
+			tempCombinationPrice = 0;
+		}
+	}
+
+	private static void calculateCombination(int currentMenuCounter, ArrayList<Food> foodStorageList) 
+	{	
+		for (int i = currentMenuCounter+1; i<menu.size();i++)
+		{
+			if (i < menu.size()-1)
+			{
+				for(Food f2 :menu.get(currentMenuCounter+1))
+				{
+					tempCombinationPrice += Double.parseDouble(f2.getPrice());
+					foodStorageList.add(f2);
+					calculateCombination(currentMenuCounter+1, foodStorageList);
+					foodStorageList.remove(f2);
+					tempCombinationPrice -= Double.parseDouble(f2.getPrice());
+				}
+			}
+			else
+			{
+				for(Food f3 :menu.get(currentMenuCounter+1))
+				{
+					//double tempPrice;
+					tempCombinationPrice += Double.parseDouble(f3.getPrice());
+					foodStorageList.add(f3);
+					
+					//tempPrice = tempCombinationPrice;
+					
+					if (tempCombinationPrice <= Double.parseDouble(userInput.get("budget")) && tempCombinationPrice> 0 && foodStorageList.size() == menu.size())
+					{
+						tmpFoodCombination.add(foodStorageList);
+						ArrayList CopyOffoodStorageList = new ArrayList(foodStorageList);
+						Combination combination = new Combination(CopyOffoodStorageList);
+						tmpFoodCombinationList.add(combination);
+					}
+					
+					tempCombinationPrice -= Double.parseDouble(f3.getPrice());
+					foodStorageList.remove(f3);
+				}
+			}
+		}
+	}
+
+	public void setAppetizerList(ArrayList<Appetizer> appList)
+	{
+		this.appetizerList = appList;	
+	}
+	public void setMainDishList(ArrayList<MainDish> mainList)
+	{
+		this.mainDishList = mainList;	
+	}
+	public void setDrinkList(ArrayList<Drinks> drList)
+	{
+		this.drinkList = drList;	
+	}
+	public void setDessertList(ArrayList<Dessert> deList)
+	{
+		this.dessertList = deList;	
+	}
+
 }
